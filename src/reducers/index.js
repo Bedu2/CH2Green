015e098daaf49ed21a9214a49b37f884f875f012
RodolfoSaldivar <rodolfo.saldivar@udem.edu>
2018-10-04 11:40:59 -0500

    Revert "Necesito ayuda con Git"

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import UsuariosReducers from './UsuariosReducers';
-
-export default combineReducers({
-	UsuariosReducers
-});
-
-
--- src/types/UsuariosTypes.js
+++
@@ -1,8 +0,0 @@
-export const TRAER = 'usuarios_traer';
-export const EXITO = 'usuarios_exito';
-export const FALLO = 'usuarios_fallo';
-export const EDITAR_NOMBRE = "usuarios_editar_nombre";
-export const EDITAR_APELLIDOPATERNO = 'usuarios_editar_apellidopaterno';
-export const EDITAR_APELLIDOMATERNO ='usuarios_editar_apellidomaterno';
-export const EDITAR_EDAD = 'usuarios_editar_edad';
-export const REINICIAR ='usuarios_reiniciar';
