4e8b6caacebf4c31ccdc2c959052230b8e94eeeb
Ale Zúñiga <aleelay@gmail.com>
2018-10-04 20:29:50 -0500

    Dependientes
    Creacion de Componen†€ Dependientes

---
+++ src/components/Dependientes/index.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+ import {Link} from 'react-router-dom';
+ import {Table, Icon} from 'react-materialize';
+ import * as UsuariosActions from '../../actions/UsuariosActions';
+
+
+class Dependientes extends Component {
+
+	componentDidMount() {
+		this.props.Direccion(true);
+		this.props.TraerUno();
+	}
+
+	desplegarUsuarios = () => (
+		<div className='dependientes'>
+		
+			<Table hoverable={true}>
+						<thead>
+							<tr>
+								<th>Nombre</th>
+								<th>Apellido P</th>
+								<th>Apellido M</th>
+								<th>Edad</th>
+								<th>Opciones</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							{
+								this.props.usuarios.map((elem, index) =>(
+									<tr key={elem._id}>
+										<td>{elem.nombre}</td>
+										<td>{elem.apellidos.paterno}</td>
+										<td>{elem.apellidos.materno}</td>
+										<td>{elem.edad}</td>
+										<td><Link to={`Usuarios/${elem._id}`} ><Icon> face </Icon></Link> 
+										<br/><Link to={`/EditarUsuario/${elem._id}`} ><Icon> create </Icon></Link> 
+										<br/>
+										<Link to={`/EliminarUsuario/${elem._id}`} ><Icon> delete </Icon></Link> </td>
+
+									</tr>
+									))
+							}
+							
+						</tbody>
+				
+				</Table>
+		</div>
+	);
+
+	render() {
+		return (
+			<div>
+					{this.desplegarUsuarios()}
+			</div>
+		);
+	}
+}
+
+
+const mapStateToProps =({ UsuariosReducers }) =>{
+	
+	return UsuariosReducers;
+}
+
+ export default connect(mapStateToProps, UsuariosActions)(Dependientes);
